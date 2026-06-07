b4508682283d39d9b60df5858f532fcfc375856b
tanaymishra26 <tanaymishra80@gmail.com>
2026-06-07 23:26:06 +0530

    Day 7

---
+++ Week_2/Day_7/26.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int factorial(int n)
+{
+    if (n==1)
+    {
+        return 1;
+    }
+    return (n * factorial(n-1));
+}
+int main()
+{
+    int n;
+    cin>>n;
+
+    if (n==0) cout<<0;
+    cout << factorial(n);
+    
+    return 0;
+}
