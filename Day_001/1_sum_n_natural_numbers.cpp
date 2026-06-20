2cc3aaa109409067ca5a3f201dda8dbef54521bc
tanaymishra26 <tanaymishra80@gmail.com>
2026-06-20 21:38:30 +0530

    Day 20

--- Day_001/1_sum_n_natural_numbers.cpp
+++ Day_001/1_sum_n_natural_numbers.cpp
@@ -9,6 +9,6 @@ int main()
         sum += i;
     }
     cout << sum;
-
+    
     return 0;
 }
---
+++ Day_020/78.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[10][10];
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            cin >> a[i][j];
+
+    bool symmetric = true;
+
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            if(a[i][j] != a[j][i])
+                symmetric = false;
+
+    if(symmetric)
+        cout << "Symmetric Matrix";
+    else
+        cout << "Not Symmetric Matrix";
+
+    return 0;
+}
