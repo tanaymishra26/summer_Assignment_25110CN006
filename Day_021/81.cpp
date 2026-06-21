9f70e9d71487ff32018f418454d8278a10754c61
tanaymishra26 <tanaymishra80@gmail.com>
2026-06-21 12:17:20 +0530

    Day 21

--- Day_021/81.cpp
+++ Day_021/81.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 using namespace std;
-int main(){char str[100];cin.getline(str,100);int len=0;while(str[len]!='\0')len++;cout<<"Length = "<<len;return 0;}
+
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    int length = 0;
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    cout << "Length of string = " << length;
+
+    return 0;
+}
